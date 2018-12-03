aed5e7da36fdd3994c01f2b4c99f70960bc3235d
bichi0812 <45431543+bichi0812@users.noreply.github.com>
2018-12-03 22:32:46 +0530

    Add files via upload

--- get_site_code.py
+++ get_site_code.py
@@ -1,13 +1,23 @@
 import urllib, urllib2, traceback, hashlib
 
 def get_website_health(url):
-    status_code = urllib.urlopen(url).getcode()
+    status_code = ""
+    try:
+        status_code = urllib.urlopen(url).getcode()
+    except Exception as e:
+        print "Couldn't fetch status code of the url"
+        print traceback.format_exc()
     return status_code
 
 def getHash(the_page):
     return hashlib.sha224(the_page).hexdigest()
 
 def get_site_content(url):
-    data = urllib2.urlopen(url)
-    content = data.read()
+    content = ""
+    try:
+        data = urllib2.urlopen(url)
+        content = data.read()
+    except Exception as e:
+        print "Couldn't Fetch html content of the page"
+        print traceback.format_exc()
     return content
