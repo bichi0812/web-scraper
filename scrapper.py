2feeb89511da88724593cd3b6e1dc0782f709477
bichi0812 <45431543+bichi0812@users.noreply.github.com>
2018-12-11 22:39:19 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -7,12 +7,13 @@
 ##################################################################################
 from get_site_code import *
 from bs4 import BeautifulSoup
-import pandas
+import pandas as pd
 import re
 from datetime import datetime
 
 
 url = "https://blog.feedspot.com/machine_learning_blogs/"
+url = "https://firstsiteguide.com/what-is-blog/"
 
 
 def visible(element):
@@ -25,6 +26,34 @@ def visible(element):
 def get_text(page):
     # getting meta properties
     title = soup.title
+    link_texts = re.findall(r"<a.*>(.*)</a>",page,re.M)
+    #for i in texts: print i
+    para_texts = re.findall(r"<p.*</p>",page,re.M)
+    #filtering resultset
+    result_set_text = [re.sub(r"<.*?>","",x) for x in para_texts]
+    # now we got all the texts
+    # now we can extract all the headings
+    heading_texts = re.findall(r"<h.*</h.*>",page,re.M)
+    #filtering resultset
+    result_set_heading = [re.sub(r"<.*?>","",x) for x in heading_texts]
+
+    return result_set_text+result_set_heading
+def tokenize_words(result):
+    tokens = [re.sub(r'(.*)(\n+)',r'\1',x) for x in result]
+    # tokens = [y.strip() for y in tokens if y.encode('utf-8') not in ['\n','']]
+    tokens = [y.strip() for y in tokens]
+    # filtering the urls
+    tokens = [y for y in tokens if bool(re.search(r'.*http.*',y)) == False]
+    # Lowercase, then replace any non-letter, space, or digit character in the headlines.
+    tokens = [re.sub(r'[^\w\s\d]','',h.lower()) for h in tokens]
+    # Replace sequences of whitespace with a space character.
+    tokens = [re.sub("\s+", " ", h) for h in tokens]
+
+    tokens = [x for x in tokens if x not in ['','\n']]
+    tokens = [x for x in tokens if not bool(re.match(r"\d+\w+|\w+\d+|\w+\d+\w+",x))==True]
+    for t in tokens:print tokens
+    unique_words = list(set(" ".join(tokens).split(" ")))
+    return unique_words
 if __name__ == "__main__":
     # collect data of website whether it is active ot not
     ret_code = get_website_health(url)
@@ -46,7 +75,8 @@ if __name__ == "__main__":
         # Extracting all possible urls
         #links = [l.get('href') for l in soup.find_all('a') if bool(re.match(r'http.*',l.get('href'),re.I))]
         links = set(re.findall(r'href="(http.*?)"',the_page,re.M))
-        for i in links: print i
+        #for i in links: print i
+
 
         # preparing timestamp
         current_time = datetime.now()
@@ -63,22 +93,12 @@ if __name__ == "__main__":
         # print soup.text.strip()
 
 
-        result = filter(visible, data)
+        # result = filter(visible, data)
+        result = get_text(the_page)
 
         # print result
-        tokens = [re.sub(r'(.*)(\n+)',r'\1',x) for x in result]
-        tokens = [y.strip() for y in tokens if y.encode('utf-8') not in ['\n','']]
-
-        # filtering the urls
-        tokens = [y for y in tokens if bool(re.search(r'.*http.*',y)) == False]
-        # Lowercase, then replace any non-letter, space, or digit character in the headlines.
-        tokens = [re.sub(r'[^\w\s\d]','',h.lower()) for h in tokens]
-        # Replace sequences of whitespace with a space character.
-        tokens = [re.sub("\s+", " ", h) for h in tokens]
-
-        tokens = [x for x in tokens if x not in ['','\n']]
-        unique_words = list(set(" ".join(tokens).split(" ")))
-        print tokens
+        unique_words = tokenize_words(result)
+        #print tokens
         # We've reduced the number of columns in the matrix a bit.
         # print(make_matrix(' '.join(tokens), unique_words))
         # print tokens
@@ -92,19 +112,13 @@ if __name__ == "__main__":
 
         # Remove stopwords from the vocabulary.
         unique_words = [w for w in unique_words if w not in stopwords]
-        print unique_words
-
-
-        # # print(make_matrix(' '.join(tokens), unique_words))
-        # def with_sklearn(tokens):
-        #     vectorizer = CountVectorizer(lowercase=True, stop_words="english")
-        #     matrix = vectorizer.fit_transform(tokens)
-        #     print(matrix.todense())
-        # with_sklearn([" ".join(tokens)])
-
         # we will try to count number of words and put in pandas
-
         # key will the word and value will be the count of that word
-        word_count_dict = {x:len(re.findall(x,the_page)) for x in set(unique_words)}
+        word_count_dict = [{x:len(re.findall(x,the_page))} for x in set(unique_words)]
+        # sorting according to max accurance
+        sorted_wordcount = sorted(word_count_dict,key = lambda x:x.values(),reverse=True)
+        print sorted_wordcount
 
-        print word_count_dict
+
+
+        # print word_count_dict
